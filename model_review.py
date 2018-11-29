a8be58fe9d306f726900fedaad6b0b2784133c11
howarder3 <howarder3@gmail.com>
2018-11-29 12:04:33 +0800

    update

--- model_review.py
+++ model_review.py
@@ -19,6 +19,7 @@ class CoopNets(object):
                  gen_step_size=0.1, gen_sample_steps=0, gen_refsig=0.3,
                  data_path='/tmp/data/', log_step=10, category='rock',
                  sample_dir='./synthesis', model_dir='./checkpoints', log_dir='./log', test_dir='./test'):
+				 
 		self.type = net_type #底下會用net_type決定z_size
 		self.num_epochs = num_epochs # 200 epochs
 		self.batch_size = batch_size # 100 batches
@@ -51,3 +52,67 @@ class CoopNets(object):
             self.z_size = 100
         elif self.type == 'object_small':
             self.z_size = 2
+			
+		self.syn = tf.placeholder(shape=[None, self.image_size, self.image_size, 3], dtype=tf.float32, name='syn') # placeholder 放syn size*size*3
+        self.obs = tf.placeholder(shape=[None, self.image_size, self.image_size, 3], dtype=tf.float32, name='obs') # placeholder 放obs size*size*3
+        self.z = tf.placeholder(shape=[None, self.z_size], dtype=tf.float32, name='z') # placeholder 放z size
+		
+		self.debug = False
+		
+	def build_model(self):
+        self.gen_res = self.generator(self.z, reuse=False) # 拿出placeholder中的size(第一層conv的初始size)	
+		'''
+		def generator(self, inputs, reuse=False, is_training=True):
+			with tf.variable_scope('gen', reuse=reuse):
+				if self.type == 'object':
+					inputs = tf.reshape(inputs, [-1, 1, 1, self.z_size])
+					convt1 = convt2d(inputs, (None, self.image_size // 16, self.image_size // 16, 512), kernal=(4, 4)
+									 , strides=(1, 1), padding="VALID", name="convt1")
+					convt1 = tf.contrib.layers.batch_norm(convt1, is_training=is_training)
+					convt1 = leaky_relu(convt1)
+
+					convt2 = convt2d(convt1, (None, self.image_size // 8, self.image_size // 8, 256), kernal=(5, 5)
+									 , strides=(2, 2), padding="SAME", name="convt2")
+					convt2 = tf.contrib.layers.batch_norm(convt2, is_training=is_training)
+					convt2 = leaky_relu(convt2)
+
+					convt3 = convt2d(convt2, (None, self.image_size // 4, self.image_size // 4, 128), kernal=(5, 5)
+									 , strides=(2, 2), padding="SAME", name="convt3")
+					convt3 = tf.contrib.layers.batch_norm(convt3, is_training=is_training)
+					convt3 = leaky_relu(convt3)
+
+					convt4 = convt2d(convt3, (None, self.image_size // 2, self.image_size // 2, 64), kernal=(5, 5)
+									 , strides=(2, 2), padding="SAME", name="convt4")
+					convt4 = tf.contrib.layers.batch_norm(convt4, is_training=is_training)
+					convt4 = leaky_relu(convt4)
+
+					convt5 = convt2d(convt4, (None, self.image_size, self.image_size, 3), kernal=(5, 5)
+									 , strides=(2, 2), padding="SAME", name="convt5")
+					convt5 = tf.nn.tanh(convt5)
+
+					return convt5
+				else:
+					return NotImplementedError		
+		'''
+		obs_res = self.descriptor(self.obs, reuse=False)
+        syn_res = self.descriptor(self.syn, reuse=True)
+		'''
+		def descriptor(self, inputs, reuse=False):
+			with tf.variable_scope('des', reuse=reuse):
+				if self.type == 'object':
+					conv1 = conv2d(inputs, 64, kernal=(5, 5), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
+								   name="conv1")
+
+					conv2 = conv2d(conv1, 128, kernal=(3, 3), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
+								   name="conv2")
+
+					conv3 = conv2d(conv2, 256, kernal=(3, 3), strides=(1, 1), padding="SAME", activate_fn=leaky_relu,
+								   name="conv3")
+
+					fc = fully_connected(conv3, 100, name="fc")
+
+					return fc
+				else:
+					return NotImplementedError
+		'''
+		
